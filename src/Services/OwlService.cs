d2f9d52812fc723734d7ae7241af96bb5e899cde
ZeroZeroOne <jeffrey.smets@gmail.com>
2019-05-09 21:06:01 +0200

    getting correct week

--- src/Services/OwlService.cs
+++ src/Services/OwlService.cs
@@ -19,26 +19,74 @@ namespace Next_OWL.Services
         private async Task<dynamic> GetSchedule()
         {
             var request = await this.httpClient.GetAsync("/schedule");
-            return request.Content.ReadAsAsync<JObject>();
+            return await request.Content.ReadAsAsync<JObject>();
         }
 
         public async Task<NextGame> GetNextGame()
         {
-            return new NextGame
-            {
-                TeamOne = "a team",
-                TeamTwo = "a second team",
-                Date = DateTime.Now
-            };
+            var now = DateTime.Now;
+            var start = GetUtcStamp(now);
 
-            var owlSchedule = await GetSchedule();
+            var inAWeek = now.AddDays(7);
+            var end = GetUtcStamp(inAWeek);
+
+
+            var owlSchedule = (await GetSchedule()).data;
+
+            dynamic currentWeek = null;
+
             foreach (dynamic stage in owlSchedule.stages)
             {
                 foreach (dynamic week in stage.weeks)
                 {
+                    var stageWeekStart = (double)week.startDate;
+                    var stageEndWeek = (double)week.endDate;
 
+                    if (stageWeekStart.IsBetween(start, end) || stageEndWeek.IsBetween(start, end))
+                    {
+                        currentWeek = week;
+                        break; ;
+                    }
                 }
             }
+
+            if (currentWeek == null)
+            {
+                return null;
+            }
+
+            foreach (dynamic match in currentWeek.matches)
+            {
+                var matchStart = (double)match.startDateTS;
+                if (matchStart.IsBetween(start, end))
+                {
+                    return new NextGame
+                    {
+                        TeamOne = match.competitors[0].name,
+                        TeamTwo = match.competitors[1].name,
+                        Date = DateTime.Parse(match.startDate)
+                    };
+                }
+            }
+
+            return null;
+        }
+
+        private double GetUtcStamp(DateTime dateTime)
+        {
+            return dateTime.ToUniversalTime()
+                .Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc))
+                .TotalMilliseconds;
+        }
+
+
+    }
+
+    public static class ExtensionMethods
+    {
+        public static bool IsBetween(this double testValue, double min, double max)
+        {
+            return testValue >= min && testValue <= max;
         }
     }
 
