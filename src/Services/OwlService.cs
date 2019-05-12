4860e77be4167dc4ad0c806fc4411ed675047329
ZeroZeroOne <jeffrey.smets@gmail.com>
2019-05-12 20:33:44 +0200

    working correct timestamps

--- src/Services/OwlService.cs
+++ src/Services/OwlService.cs
@@ -28,13 +28,13 @@ namespace Next_OWL.Services
 
         public async Task<NextGame> GetNextGame()
         {
-            var now = DateTime.Now.AddHours(-9);
-            var start = now.ToUtcStamp();
+            var start = DateTimeOffset.Now.ToUnixTimeMilliseconds();
 
             var owlSchedule = (await GetSchedule()).Data;
 
             var match = owlSchedule.Stages
                         .SelectMany(s => s.Matches)
+                        .Where(m => m.Competitors[0] != null)
                         .OrderBy(m => m.StartDateTS)
                         .FirstOrDefault(m => m.StartDateTS >= start);
 
@@ -52,20 +52,4 @@ namespace Next_OWL.Services
 
         }
     }
-
-    public static class ExtensionMethods
-    {
-        public static bool IsBetween(this double testValue, double min, double max)
-        {
-            return testValue >= min && testValue <= max;
-        }
-
-        public static double ToUtcStamp(this DateTime dateTime)
-        {
-            return dateTime.ToUniversalTime()
-                .Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc))
-                .TotalMilliseconds;
-        }
-    }
-
 }
