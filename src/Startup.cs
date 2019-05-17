8243f9c5d04fa52d395598b2660478d679c3f025
Jeffrey <jeffrey.smets@gmail.com>
2019-05-17 15:12:35 +0200

    http, 8080

--- src/Startup.cs
+++ src/Startup.cs
@@ -43,13 +43,7 @@ namespace Next_OWL
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
